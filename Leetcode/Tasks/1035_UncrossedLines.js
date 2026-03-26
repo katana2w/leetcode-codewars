8e422be27cc29ab327870ab57a8c465c7ac548aa
Yura Kulyk <yuri.katana.kulyk@gmail.com>
2026-03-26 22:30:33 +0400

    new leetcode tasks

--- Leetcode/Tasks/1035_UncrossedLines.js
+++ Leetcode/Tasks/1035_UncrossedLines.js
@@ -78,3 +78,25 @@ var maxUncrossedLines = function(A, B) {
 
 // console.log(maxUncrossedLines([1,4,2], [1,2,4]))
 console.log(maxUncrossedLines([1,2],   [1,1]))
+
+
+function maxUncrossedLines(nums1: number[], nums2: number[]): number {
+    const n = nums1.length;
+    const m = nums2.length;
+
+    const dp: number[][] = Array.from({ length: n + 1 }, () =>
+        new Array(m + 1).fill(0)
+    );
+
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= m; j++) {
+            if (nums1[i - 1] === nums2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[n][m];
+};
--- Leetcode/Tasks/1143_LongestCommonSubsequence.js
+++ Leetcode/Tasks/1143_LongestCommonSubsequence.js
@@ -76,3 +76,24 @@ var longestCommonSubsequence = function(text1, text2) {
 };
 
 console.log(longestCommonSubsequence("XMJYAUZ", "MZJAWXU"));
+
+function longestCommonSubsequence(text1: string, text2: string): number {
+    const m = text1.length;
+    const n = text2.length;
+
+    const dp: number[][] = Array.from({ length: m + 1 }, () =>
+        new Array(n + 1).fill(0)
+    );
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (text1[i - 1] === text2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[m][n];
+};
